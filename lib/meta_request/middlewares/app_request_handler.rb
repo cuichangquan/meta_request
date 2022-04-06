1617a504da491bd7b1b6a6ae263e22f796433556
changquan.cui <ccq1170@gmail.com>
2022-04-07 00:18:54 +0900

    just get controller and action names and save to log/meta_request.log

--- lib/meta_request/middlewares/app_request_handler.rb
+++ lib/meta_request/middlewares/app_request_handler.rb
@@ -18,7 +18,21 @@ module MetaRequest
         end
         raise
       ensure
-        Storage.new(app_request.id).write(app_request.events.to_json) unless app_request.events.empty?
+        # Storage.new(app_request.id).write(app_request.events.to_json) unless app_request.events.empty?
+        unless app_request.events.empty?
+          Storage.new(app_request.id).write(app_request.events.to_json)
+          controller_event = app_request.events.find do |event|
+            event.name == "process_action.action_controller"
+          end
+          controller_name = controller_event.payload[:controller]
+          action_name = controller_event.payload[:action]
+          event_time = controller_event.time.strftime("%Y-%m-%d %H:%M:%S")
+          output = "#{event_time} #{controller_name}##{action_name}"
+          # puts output to rails root/log/meta_request.log
+          File.open("#{Rails.root}/log/meta_request.log", "a") do |f|
+            f.puts output
+          end
+        end
       end
     end
   end
